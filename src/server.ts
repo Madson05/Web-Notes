1f3564106a8da3cb3ca51598fbb0aea70b29ebe1
madson <madsonsilva2005@gmail.com>
2023-07-14 17:00:39 -0300

    :sparkle: feat: implementadas as paginas html de forma estatica no servidor.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
+import url from "url";
+import path from "path";
 
-dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 const app = express();
 
+const currentPath = url.fileURLToPath(import.meta.url);
+const dirPublic = path.join(currentPath, "../../public");
+app.use(express.static(dirPublic));
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
