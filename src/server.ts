5ad348e2fa03346c177c37b9d211f8272762e39c
madson <madsonsilva2005@gmail.com>
2023-07-18 10:32:00 -0300

    :sparkle: feat: implementado servidor, feito uso do socket.io e os eventos já estão sendo emitidos pelo front-end

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 
 const port = 3000;
@@ -9,6 +11,15 @@ const app = express();
 const currentPath = url.fileURLToPath(import.meta.url);
 const dirPublic = path.join(currentPath, "../../public");
 app.use(express.static(dirPublic));
-app.listen(port, () => {
+
+const serverHttp = http.createServer(app);
+
+serverHttp.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+});
+
+const io = new Server(serverHttp)
+
+io.on("connection", (socket) => {
+  console.log("cliente conectado")
 });
