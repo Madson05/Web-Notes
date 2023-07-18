3ea94b3d2cc91a1c77295916e4c8faa2bb230616
madson <madsonsilva2005@gmail.com>
2023-07-18 11:13:39 -0300

    :sparkles: feat: adicionada comunicação entre back e front nos eventos de teclado.

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+const textoEditor = document.getElementById("editor-texto");
+
+textoEditor.addEventListener("input", () => {
+  socket.emit("texto-editor", textoEditor.value);
+});
--- src/socket-back.ts
+++ src/socket-back.ts
@@ -2,4 +2,8 @@ import io from "./server";
 
 io.on("connection", (socket) => {
   console.log(`Socket conectado: ${socket.id}`);
+
+  socket.on("texto-editor", (texto) => {
+    console.log(texto);
+  });
 });
