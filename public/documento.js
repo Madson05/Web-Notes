2e3ef7160c5acf12197559cebdeffcf3f812a129
madson <madsonsilva2005@gmail.com>
2023-07-21 16:11:05 -0300

    :sparkles: feat: adicionado edição do titulo do documento de acordo com o parametro passado ao acessar o mesmo via URL.

--- public/documento.js
+++ public/documento.js
@@ -1,7 +1,13 @@
-
 import { emitirTextoEditor } from "./socket-front-documento.js";
 
+const parametros = new URLSearchParams(window.location.search);
+const nomeDocumento = parametros.get("nome");
+
 const textoEditor = document.getElementById("editor-texto");
+const tituloDocumento = document.getElementById("titulo-documento");
+
+tituloDocumento.textContent = nomeDocumento || "Documento sem título";
+
 
 textoEditor.addEventListener("input", () => {
   emitirTextoEditor(textoEditor.value);
